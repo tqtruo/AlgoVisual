9e8488a7977ae02d053896373d360dd96e912ed8
tqtruo <tqtruo@gmail.com>
2020-02-03 16:41:55 -0600

    Added function to generate an array of numbers and shuffle them

--- src/Components/Sorting.js
+++ src/Components/Sorting.js
@@ -1,7 +1,55 @@
 import React from "react";
 
-const Sorting = () => {
-	return <div className="sorting">Welcome to Sorting</div>;
-};
+class Sorting extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			numArr: []
+		};
+	}
+
+	componentDidMount() {
+		this.fillArray();
+	}
+
+	/* The shuffle function uses the Fisher-Yates shuffle method
+		https://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+	*/
+	shuffle(arr) {
+		for (let i = arr.length - 1; i > 0; i--) {
+			let randomIndex = Math.floor(Math.random() * (i + 1));
+
+			let temp = arr[i];
+			arr[i] = arr[randomIndex];
+			arr[randomIndex] = temp;
+		}
+
+		return arr;
+	}
+
+	fillArray() {
+		const newArray = [];
+		for (let i = 1; i <= 100; i++) {
+			newArray.push(i);
+		}
+
+		const shuffledArr = this.shuffle(newArray);
+
+		this.setState({
+			numArr: shuffledArr
+		});
+	}
+
+	render() {
+		//console.log("the state: " + this.state.numArr);
+		return (
+			<div className="sorting">
+				{this.state.numArr.map((num, index) => (
+					<div key={index}>{num}</div>
+				))}
+			</div>
+		);
+	}
+}
 
 export default Sorting;
